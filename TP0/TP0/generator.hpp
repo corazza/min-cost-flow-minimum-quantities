35b116dfc2cbe158190d6e4f231e158cf84527b9
Jan Corazza <corazzajan@gmail.com>
2021-12-06 16:09:28 +0100

    Drop files from .gitignore

---
+++ TP0/TP0/generator.hpp
@@ -0,0 +1,85 @@
+/* Our problem network generator
+ */
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "network.hpp"
+
+// TODO look at those three parameters other generators use, make non-uniform generator based on
+// them
+
+struct Parameters {
+    unsigned int n_nodes;
+    unsigned int flow_value;
+    unsigned int cost_max;      // edge cost is in [0, cost_max]
+    unsigned int capacity_max;  // variable lower bound (minimum qunatity) is in [0, capacity_max]
+                                // upper bound (capacity) is in [1, capacity_max]
+
+    Parameters(unsigned int n_nodes, unsigned int flow_value, unsigned int cost_max,
+               unsigned int capacity_max)
+        : n_nodes(n_nodes),
+          flow_value(flow_value),
+          cost_max(cost_max),
+          capacity_max(capacity_max) {}
+};
+
+struct ParametersOne {
+    unsigned int n_nodes;
+    unsigned int flow_value;
+    unsigned int cost_max;      // edge cost is in [0, cost_max]
+    unsigned int capacity_max;  // variable lower bound (minimum qunatity) is in [0, capacity_max]
+                                // upper bound (capacity) is in [1, capacity_max]
+
+    ParametersOne(unsigned int n_nodes, unsigned int flow_value, unsigned int cost_max,
+                  unsigned int capacity_max)
+        : n_nodes(n_nodes),
+          flow_value(flow_value),
+          cost_max(cost_max),
+          capacity_max(capacity_max) {}
+};
+
+std::pair<vertex_key, vertex_key> add_random_edge(Network& network, Parameters p);
+
+// TODO this generator is terrible, but makes valid problem instances
+// Builds Network of p.n_nodes nodes
+// - edges are created at random, but a source->sink path is ensured, etc.
+// - assigns each edge a random cost, capacity, and minimum quantity (as defined by p)
+Network generate_instance(Parameters p);
+
+// data structure used by generate_instance2
+struct Blueprint {
+    unsigned int node_counter;
+    vertex_key source;
+    vertex_key sink;
+
+    std::unordered_map<vertex_key, std::set<vertex_key> > outgoing;
+    std::unordered_map<vertex_key, std::set<vertex_key> > incoming;
+
+    vertex_key add_node();
+    void add_edge(vertex_key v_from, vertex_key v_to);
+    void remove_edge(vertex_key v_from, vertex_key v_to);
+    bool exists_edge(vertex_key v_from, vertex_key v_to);  // considers direction
+    vertex_key random_regular_node();
+
+    Blueprint() : node_counter(0) {
+        this->source = this->add_node();
+        this->sink = this->add_node();
+        vertex_key v = this->add_node();
+        this->add_edge(this->source, v);
+        this->add_edge(v, this->sink);
+    }
+};
+
+#define BP_ACTION_PAR 1
+#define BP_ACTION_SEQ 2
+
+Network blueprint_to_network(Blueprint blueprint, Parameters p);
+void execute_action(Blueprint& blueprint, vertex_key node, int action);
+// better generator
+Network generate_instance2(Parameters p);
+
+// generator from paper (1)
+Network generate_instance_paper_one(ParametersOne p);
+
+#endif
---
+++ TP0/TP0/network.cpp
@@ -0,0 +1,52 @@
+#include "network.hpp"
+
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <stack>
+
+void Network::add_edge(vertex_key v_from, vertex_key v_to, int cost, int capacity,
+                       int minimum_quantity, bool vlb) {
+    edge_key edge = get_edge_key((vertex_key)v_from, (vertex_key)v_to);
+
+    this->costs[edge] = cost;
+    this->capacities[edge] = capacity;
+    this->minimum_quantities[edge] = minimum_quantity;
+    this->vlb[edge] = vlb;
+
+    this->outgoing[v_from].insert(v_to);
+    this->incoming[v_to].insert(v_from);
+}
+
+void Network::remove_edge(vertex_key v_from, vertex_key v_to) {
+    assert(!"Network::remove_edge unimplemented!");
+}
+
+bool Network::exists_edge(vertex_key v_from, vertex_key v_to) {
+    edge_key edge = get_edge_key((vertex_key)v_from, (vertex_key)v_to);
+    return this->costs.find(edge) != this->costs.end();
+}
+
+bool Network::exists_path(vertex_key v_from, vertex_key v_to) {
+    std::set<vertex_key> visited;
+    std::stack<vertex_key> to_visit;
+    to_visit.push(v_from);
+    while (!to_visit.empty()) {
+        vertex_key visiting = to_visit.top();
+        to_visit.pop();
+        if (visiting == v_to) {
+            return true;
+        }
+        visited.insert(visiting);
+        for (auto v : this->outgoing[visiting]) {
+            if (visited.find(v) == visited.end()) {
+                to_visit.push(v);
+            }
+        }
+    }
+    return false;
+}
+
+int flow_value(Flow& f, Network& network) { assert(!"flow_value unimplemented!"); }
+
+int flow_cost(Flow& f, Network& network) { assert(!"flow_cost unimplemented!"); }
